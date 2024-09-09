9a314eccdae3de514162aef440fa7b6df7b13918
kahkim <kahkim@dgu.ac.kr>
2024-09-09 16:05:08 +0900

    aaa

--- job_seq_sample.py
+++ job_seq_sample.py
@@ -1,16 +1,31 @@
 import scheduling_lib as slib
-# import pandas as pd
+import pandas as pd
 
 ptime_file = 't_500_20_mon.csv'
 job_seq = [4,2,3,5,1]
 
-# ptimes = pd.read_csv(ptime_file, index_col='JobID', nrows=len(job_seq))
-# print(ptimes)
-# ptimes = ptimes.sort_values(by=['M1'], ascending=True)
-# print(ptimes)
-# job_seq = ptimes.index.values
-# print(job_seq)
+
+print(pd.read_csv(ptime_file))
+
+ptimes = pd.read_csv(ptime_file, index_col='JobID', nrows=len(job_seq))
+print(ptimes)
+ptimes = ptimes.sort_values(by=['M1'], ascending=True)
+print(ptimes)
+job_seq = ptimes.index.values
+print(job_seq)
 
 
 makespan = slib.schedule(ptime_file, job_seq)
 print(makespan)
+
+
+
+
+import pandas as pd
+import scheduling_lib as slib
+
+ptime_file = 't_500_20_mon.csv'
+
+
+
+pd.read_csv(ptime_file, index_col='JobID')
